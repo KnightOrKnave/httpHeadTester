8b99af00c788dbaa2252c9eb13febb90aafeab11
kqhome <a11dc094@dhw.ac.jp>
2019-08-22 20:40:07 +0900

    ugoita

--- sidedoor/src/routes/index.js
+++ sidedoor/src/routes/index.js
@@ -1,4 +1,4 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 var Redis = require("ioredis");
 
@@ -6,22 +6,22 @@ const databaseHost = process.env.DB_HOST || "localhost";
 const databasePort = process.env.DB_PORT || 6379;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get("/", function(req, res, next) {
   let kvarray = [];
   (async () => {
     try {
       const redis = new Redis(databasePort, databaseHost);
       keys = await redis.keys("*");
-      for (var x of keys.sort) {
+      for (var x of keys) {
         var t = await redis.get(x);
-        kvarray.push({time:x,header:t})
+        kvarray.push({ time: x, header: t });
       }
       redis.disconnect();
+      res.render("index", { title: "Express", arr: kvarray });
     } catch (err) {
       console.log(err);
       redis.disconnect();
     } finally {
-      res.render('index', { title: 'Express' ,arr: kvarray});
     }
   })();
 });
