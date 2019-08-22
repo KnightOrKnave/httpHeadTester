0c953d606c17da7966b4b7d24718c3824755b7bf
kqhome <a11dc094@dhw.ac.jp>
2019-08-22 20:15:46 +0900

    return result

--- sidedoor/src/routes/index.js
+++ sidedoor/src/routes/index.js
@@ -7,20 +7,21 @@ const databasePort = process.env.DB_PORT || 6379;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  let kvarray = [];
   (async () => {
     try {
       const redis = new Redis(databasePort, databaseHost);
       keys = await redis.keys("*");
-      kvarray = [];
       for (var x of keys.sort) {
         var t = await redis.get(x);
+        kvarray.push({time:x,header:t})
       }
       redis.disconnect();
     } catch (err) {
       console.log(err);
       redis.disconnect();
     } finally {
-      res.render('index', { title: 'Express' });
+      res.render('index', { title: 'Express' ,arr: kvarray});
     }
   })();
 });
